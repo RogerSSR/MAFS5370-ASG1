e7bf2522d826675a60aeaaa81b732dea7568ed95
Roger SHAO <shaorz@gmail.com>
2023-02-24 00:26:28 +0800

    Adding dependencies from Foundations-of-Reinforcement-Learning-with-Applications-in-Finance-master repo

---
+++ src/rl_lib/testMonteCarlo.py
@@ -0,0 +1,48 @@
+import random
+import unittest
+
+import src.rl_lib.iterate as iterate
+import src.rl_lib.monte_carlo as mc
+from src.rl_lib.distribution import Categorical, Choose
+from src.rl_lib.function_approx import Tabular
+from src.rl_lib.markov_process import FiniteMarkovRewardProcess, NonTerminal
+
+
+class FlipFlop ( FiniteMarkovRewardProcess [ bool ] ) :
+    '''A version of FlipFlop implemented with the FiniteMarkovProcess
+    machinery.
+
+    '''
+
+    def __init__ ( self, p: float ) :
+        transition_reward_map = {
+            b : Categorical ( { (not b, 2.0) : p, (b, 1.0) : 1 - p } )
+            for b in (True, False)
+        }
+        super ( ).__init__ ( transition_reward_map )
+
+
+class TestEvaluate ( unittest.TestCase ) :
+    def setUp ( self ) :
+        random.seed ( 42 )
+        self.finite_flip_flop = FlipFlop ( 0.7 )
+
+    def test_evaluate_finite_mrp ( self ) :
+        start = Tabular ( { s : 0.0 for s in
+                            self.finite_flip_flop.non_terminal_states } )
+        traces = self.finite_flip_flop.reward_traces ( Choose ( {
+            NonTerminal ( True ),
+            NonTerminal ( False )
+        } ) )
+        v = iterate.converged (
+                mc.mc_prediction ( traces, γ = 0.99, approx_0 = start ),
+                # Loose bound of 0.01 to speed up test.
+                done = lambda a, b : a.within ( b, 0.01 )
+        )
+
+        self.assertEqual ( len ( v.values_map ), 2 )
+
+        for s in v.values_map :
+            # Intentionally loose bound—otherwise test is too slow.
+            # Takes >1s on my machine otherwise.
+            self.assertLess ( abs ( v ( s ) - 170 ), 1.0 )
